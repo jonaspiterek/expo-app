ba51d723f2b06596b69b861b4f884f8f294b903a
Jonas Piterek <piterek@kitconcept.com>
2023-05-19 16:13:46 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./HomeScreen";
-import ProfileScreen from "./ProfileScreen";
-
-const Stack = createNativeStackNavigator();
+import Navigation from "./components/Navigation/Navigation";
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "Hello world!" }}
-        />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-      </Stack.Navigator>
+      <Navigation />
     </NavigationContainer>
   );
 };
